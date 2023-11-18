814e38a8f064837e4550c4b311d5e14304f27b37
Yongxin Chen <yongxin.chen@cloudpilots.com>
2023-11-18 02:02:48 +0100

    packages

---
+++ ML_challenge_GIT_2023.py
@@ -0,0 +1,163 @@
+# %%
+! pip3 install shap
+import numpy as np
+import pandas as pd
+import datetime
+import random
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 18})
+plt.style.use('ggplot')
+import seaborn as sns
+from scipy import stats
+from sklearn.impute import SimpleImputer
+import shap
+from sklearn.preprocessing import StandardScaler
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+
+# %%
+df = pd.read_csv(r"./GIT_data/data_set.csv")
+
+print("Number of df samples are",df.shape)
+
+categorical_features = ['State_Factor', 'building_class', 'facility_type']
+numerical_features=df.select_dtypes('number').columns
+
+
+# %%
+df.head()
+
+# %%
+plt.figure(figsize = (25,11))
+sns.heatmap(df.isna().values, xticklabels=df.columns)
+plt.title("Missing values in  Data", size=20)
+
+# %%
+missing_columns = [col for col in df.columns if df[col].isnull().any()]
+missingvalues_count =df.isna().sum()
+missingValues_df = pd.DataFrame(missingvalues_count.rename('Null Values Count')).loc[missingvalues_count.ne(0)]
+missingValues_df.style.background_gradient(cmap="Pastel1")
+
+df['year_built'] =df['year_built'].replace(np.nan, 2022)
+null_col=['energy_star_rating','direction_max_wind_speed','direction_peak_wind_speed','max_wind_speed','days_with_fog']
+imputer = SimpleImputer()
+imputer.fit(df[null_col])
+data_transformed = imputer.transform(df[null_col])
+df[null_col] = pd.DataFrame(data_transformed)
+
+# %%
+plt.figure(figsize = (25,11))
+sns.heatmap(df.isna().values, xticklabels=df.columns)
+plt.title("Missing values in training Data", size=20)
+
+# %%
+le = LabelEncoder()
+for col in categorical_features:
+    df[col] = le.fit_transform(df[col])
+
+# %%
+df.head()
+
+# %%
+df.describe().style.background_gradient()
+
+# %%
+import copy
+y = df["site_eui"]
+train = df.drop(["site_eui","id"],axis =1)
+trainnames = copy.deepcopy(train)
+scaler = StandardScaler()
+train = scaler.fit_transform(train)
+
+
+# %%
+train
+
+# %%
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(train, y, test_size = 0.2, random_state = 50)
+
+
+# %%
+X_test_ft=pd.DataFrame(X_test,columns=trainnames.columns)
+X_test_ft.shape()
+
+# %%
+import xgboost
+xgboost_model = xgboost.XGBRegressor(n_estimators=200, learning_rate=0.02, gamma=0, subsample=0.75,
+                           colsample_bytree=0.4, max_depth=5)
+xgboost_model.fit(X_train,y_train)
+
+# %%
+#!pip install eli5
+import eli5
+from eli5.sklearn import PermutationImportance
+
+perm = PermutationImportance(xgboost_model, random_state=1).fit(X_test, y_test)
+eli5.show_weights(perm, feature_names = trainnames.columns.tolist(), top=63)
+
+# %%
+explainer = shap.Explainer(xgboost_model)
+shap_values = explainer(X_test_ft)
+
+# %%
+#Variable Importance Plot
+shap.summary_plot(shap_values, X_test_ft,plot_type="bar")
+
+# %%
+shap.plots.bar(shap_values,max_display=12)
+
+# %%
+shap.summary_plot(shap_values, X_test_ft)
+
+# %%
+shap_interaction_values = explainer.shap_interaction_values(X_test_ft)
+shap.summary_plot(shap_interaction_values, X_test_ft)
+
+# %%
+shap.plots.waterfall(shap_values[0])
+
+# %%
+shap.plots.waterfall(shap_values[1])
+
+# %%
+explainer = shap.TreeExplainer(xgboost_model)
+shap_values = explainer.shap_values(X_test_ft)
+shap.initjs()
+def p(j):
+    return(shap.force_plot(explainer.expected_value, shap_values[j,:], X_test_ft.iloc[j,:]))
+p(0)
+
+# %%
+p(1)
+
+# %%
+expected_value = explainer.expected_value
+shap_values = explainer.shap_values(X_test_ft)[0]
+shap.decision_plot(expected_value, shap_values, X_test_ft)
+
+# %%
+shap_values = explainer.shap_values(X_test_ft)[1]
+shap.decision_plot(expected_value, shap_values, X_test_ft)
+
+# %%
+import lime
+import lime.lime_tabular
+
+explainer = lime.lime_tabular.LimeTabularExplainer(X_train,
+                    feature_names=trainnames.columns,
+                    class_names=['site_eui'],
+                    categorical_features=categorical_features,
+                    verbose=True, mode='regression')
+
+# %%
+exp = explainer.explain_instance(X_test_ft.iloc[0],
+     xgboost_model.predict, num_features=10)
+exp.as_pyplot_figure()
+
+
